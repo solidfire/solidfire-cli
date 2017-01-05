c2f4f90d1e0f244d140bfa8a9f7bebdea3c5c11c
Ariel Hoffman <ariel.hoffman@netapp.com>
2017-01-05 13:48:56 -0700

    Fixes some bugs and finishes the array parameter test.

--- element/cli/parser.py
+++ element/cli/parser.py
@@ -1,8 +1,9 @@
+from element import exceptions
 def parse_array(input_string):
     # The base case is that it is None:
     if(input_string == '' or input_string is None):
         return None
 
     # First, split the input string on ','
-    array = input_string.split(',')
+    array = str(input_string).split(',')
     return array
--- element/tests/input_suite.py
+++ element/tests/input_suite.py
@@ -12,18 +12,32 @@ def rand_string(length):
 
 def check_array_input():
     # First, make a new account.
-    print("hi")
     runner = CliRunner()
     account_name = rand_string(15)
     result = runner.invoke(cli.cli, ['-c','0','-j',"Account", "Add", '--username', account_name])
     account = jsonpickle.decode(result.output)
 
     # Next, make two volumes
+    result = runner.invoke(cli.cli, ['-c','0','-j',"Volume", "Create", '--name', rand_string(15), "--account_id", account.account_id, "--total_size", "1000000000", "--enable512e", True])
+    volume1 = jsonpickle.decode(result.output)
+    result = runner.invoke(cli.cli, ['-c','0','-j',"Volume", "Create", '--name', rand_string(15), "--account_id", account.account_id, "--total_size", "1000000000", "--enable512e", True])
+    volume2 = jsonpickle.decode(result.output)
 
+    # Now we get the volumes from the given account to make sure they're there:
+    result = runner.invoke(cli.cli, ['-c','0','-j',"Volume", "List", '--accounts', account.account_id])
+    volumes_list = jsonpickle.decode(result.output)
+    assert len(volumes_list.volumes) == 2
+
+    # Now we delete and purge them. This exercises the use of an array.
+    result = runner.invoke(cli.cli, ['-c','0','-j',"Volume", "Delete", '--volume_ids', ','.join([str(volume1.volume_id), str(volume2.volume_id)])])
+    result = runner.invoke(cli.cli, ['-c','0','-j',"Volume", "PurgeDeleted", '--volume_ids', ','.join([str(volume1.volume_id), str(volume2.volume_id)])])
+
+    # Finally, we check to make sure they're gone.
+    result = runner.invoke(cli.cli, ['-c','0','-j',"Volume", "List", '--accounts', account.account_id])
+    volumes_list = jsonpickle.decode(result.output)
+    assert len(volumes_list.volumes) == 0
 
     # Now tear down the account.
     result = runner.invoke(cli.cli, ['-c','0','-j',"Account", "Remove", '--account_id', account.account_id])
-    print(result.output)
-    # Next, verify that it happened by opening up the csv file and checking.
 
 check_array_input()
