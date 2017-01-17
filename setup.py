c3319b32a12086a15cbe7ee1ad016f1bd0ef914c
Ariel Hoffman <ariel.hoffman@netapp.com>
2017-01-17 11:52:00 -0700

    Increases the version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='solidfire-cli',
-    version='0.0.0.8',
+    version='0.0.1.9',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='SolidFire, Inc.',
