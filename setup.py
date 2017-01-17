5c32454e9df202debb3f03c4962831cc487a1e80
Ariel Hoffman <ariel.hoffman@netapp.com>
2017-01-17 10:38:00 -0700

    Adds version bumping logic.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='solidfire-cli',
-    version='0.0.1',
+    version='0.0.0.2',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='SolidFire, Inc.',
