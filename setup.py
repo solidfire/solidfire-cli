9b6c397326520b34ae0a37baffaea043a16f6755
Build Automation <dan.audette@solidfire.com>
2017-02-15 16:38:44 -0700

    Generated files changed in subrepo

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='solidfire-cli',
-    version='0.0.1.53',
+    version='0.0.1.54',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='SolidFire, Inc.',
