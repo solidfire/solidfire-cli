d6749890a218b875d483ef44ec0074c835928072
Build Automation <dan.audette@solidfire.com>
2017-04-04 08:55:18 -0600

    {"cliVersion":"", "descriptorVersion":"1.0.0.37", "generatorVersion": "1.0.0.5"}

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='solidfire-cli',
-    version='0.0.1.203',
+    version='0.0.1.204',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='SolidFire, Inc.',
