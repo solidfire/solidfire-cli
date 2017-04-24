b1b0bc5d603e1086cb36f874ea95b625b7b9d980
Ariel Hoffman <ariel.hoffman@netapp.com>
2017-04-24 11:50:43 -0600

    Provides a mechanism for passinging in a null to an array or a [] to an array.

--- element/cli/parser.py
+++ element/cli/parser.py
@@ -1,8 +1,14 @@
 from element import exceptions
 def parse_array(input_string):
     # The base case is that it is None:
-    if(input_string == '' or input_string is None):
+    if(input_string == '' or input_string == 'null' or input_string is None):
         return None
+    # If it is surrounded by brackets, we just remove the brackets:
+    if(input_string[0] == '[' and input_string[-1] == ']'):
+        input_string = input_string[1:-2]
+        # When we want an empty array.
+        if input_string == '':
+            return []
 
     # First, split the input string on ','
     array = str(input_string).split(',')
