a2f8b3157fbbdb51884d278b263eb217e7a015fa
Build Automation <nicole.cotton@netapp.com>
2017-05-01 12:04:19 -0600

    {"cliVersion":"1.4.0.323", "descriptorVersion":"1.4.0.81", "generatorVersion": "1.4.0.72"}

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='solidfire-cli',
-    version='1.4.0.322',
+    version='1.4.0.323',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='SolidFire, Inc.',
