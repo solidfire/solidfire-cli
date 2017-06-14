e5a6eb71e09c663f82ecc2209b23c96b65b46b99
Build Automation <hudson@solidfire.com>
2017-06-13 21:57:54 -0600

    {"cliVersion":"1.4.0.353", "descriptorVersion":"1.5.0.6", "generatorVersion": "1.5.0.6"}

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='solidfire-cli',
-    version='1.4.0.352',
+    version='1.4.0.353',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='SolidFire, Inc.',
