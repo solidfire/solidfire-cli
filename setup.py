eca076083cf3f0ae174bbbc2c85664b88280edaa
Adam Haid <adam.haid@solidfire.com>
2017-09-25 12:15:25 -0600

    bumping version to 1.5.0.29 for bug fixes

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='solidfire-cli',
-    version='1.5.0.28',
+    version='1.5.0.29',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='SolidFire, Inc.',
